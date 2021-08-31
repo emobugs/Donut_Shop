c19e683fb6f696638d43a4d405c6b8ed6a503d79
Dimitar Nikolov <dknikolov@gmail.com>
2021-08-31 11:05:27 +0300

    feat: add task boilerplate page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>My app</div>
+      <section class="hero">
+        <div class="hero-body">
+          <p class="title">A React Task</p>
+          <p class="subtitle">by Boom.dev</p>
+        </div>
+      </section>
+      <div class="container is-fullhd">
+        <div class="notification">
+          Edit the <code>./src</code> folder to add components.
+        </div>
+      </div>
     </div>
   );
 }
