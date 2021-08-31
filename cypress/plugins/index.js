986840a9fc96fd679bdda5488d7832703e0883e8
Dimitar Nikolov <dknikolov@gmail.com>
2021-08-31 10:48:43 +0300

    chore: initial commit with basic config

---
+++ cypress/plugins/index.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+const injectDevServer = require("@cypress/react/plugins/react-scripts");
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+module.exports = (on, config) => {
+  injectDevServer(on, config);
+  return config;
+};
--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    console.trace()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
